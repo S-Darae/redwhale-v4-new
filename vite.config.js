dfa434995329229269a4160feda9c38bd68fe526
darae <ekfo320@naver.com>
2025-10-06 15:19:11 +0900

    테스트

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,19 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/redwhale-v4-new/",
-  root: "src", // ✅ src 폴더를 루트로 인식하게 설정
+  root: "src",
   build: {
-    outDir: "../dist", // ✅ 빌드 결과물이 dist로 나가게
+    outDir: "../dist",
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "src/index.html"), // 루트 index.html
-        button: path.resolve(__dirname, "src/components/button/button.html"), // 버튼 데모
+        main: path.resolve(__dirname, "src/index.html"),
+        button: path.resolve(__dirname, "src/components/button/button.html"),
       },
     },
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "foundation/index" as *;
-        `,
-      },
+      scss: { additionalData: `@use "foundation/index" as *;` },
     },
   },
 });
