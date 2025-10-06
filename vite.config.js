f91881342e710c9d0a0f8697c3e1bce895815de3
darae <ekfo320@naver.com>
2025-10-06 15:27:40 +0900

    테스트

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,22 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/redwhale-v4-new/",
-  root: ".", // ✅ 루트를 현재 폴더로 변경
+  root: ".", // ✅ 루트는 현재 폴더
   build: {
-    outDir: "dist",
+    outDir: "dist", // ✅ dist 폴더는 루트 바로 아래로 생성
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"),
+        main: path.resolve(__dirname, "index.html"), // ✅ 루트 기준으로 변경
+        button: path.resolve(__dirname, "src/components/button/button.html"), // 예시
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/foundation/index" as *;
+        `,
       },
     },
   },
