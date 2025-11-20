99734e9b449a395807738b11998ca2f34173a799
darae <ekfo320@naver.com>
2025-11-20 12:57:56 +0900

    폴더명, 파일명 변경

---
+++ src/pages/attendance-management/attendance-management.js
@@ -0,0 +1,8 @@
+import "../common/main-menu.js";
+import "./attendance-management-field.js";
+import "./attendance-management-filter.js";
+import "./attendance-management-table.js";
+import "./attendance-management.scss";
+
+import "../../components/button/button.js";
+import "../../components/tooltip/tooltip.js";
--- src/pages/attendance/attendance.js
+++
@@ -1,8 +0,0 @@
-import "../../pages/common/main-menu.js";
-import "./attendance-field.js";
-import "./attendance-filter.js";
-import "./attendance-table.js";
-import "./attendance.scss";
-
-import "../../components/button/button.js";
-import "../../components/tooltip/tooltip.js";
